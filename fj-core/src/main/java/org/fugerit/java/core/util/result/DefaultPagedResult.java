918a5e64b5e07015a001c30e1a503bad01b6357c
Daneel <d@fugerit.org>
2017-05-11 12:26:47 +0200

    Import classes from fugerit-java old version

---
+++ fj-core/src/main/java/org/fugerit/java/core/util/result/DefaultPagedResult.java
@@ -0,0 +1,113 @@
+package org.fugerit.java.core.util.result;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class DefaultPagedResult<T> extends AbstractPagedResult<T>  implements Serializable {
+
+	/*
+	 * 
+	 */
+	private static final long serialVersionUID = -4104228790597768353L;
+
+	private int realPerPage;
+	
+	private int realCurrentPage;
+	
+	private String virtualKey;
+	
+	/*
+	 * <p>Creates a new paged result</p>
+	 * 
+	 * @param perPage			maximum number of elements in a page
+	 * @param elementCount		total number of elements in all pages
+	 * @param currentPage		current page ( range 1 - n )
+	 * @param pageElements		elements in the current page
+	 * @return					a new paged result
+	 */
+	public static <T> PagedResult<T>  newPagedResult( int perPage, long elementCount, int currentPage, List<T> pageElements ) {
+		int pageCount = calcPageCount( elementCount, perPage );
+		AbstractPagedResult<T> result = new DefaultPagedResult<T>( perPage, elementCount, currentPage, pageCount, pageElements, perPage, currentPage, null );
+		return result;
+	}
+	
+	/*
+	 * <p>Creates a new paged result</p>
+	 * 
+	 * @param perPage			maximum number of elements in a page
+	 * @param elementCount		total number of elements in all pages
+	 * @param currentPage		current page ( range 1 - n )
+	 * @param pageElements		elements in the current page
+	 * @return					a new paged result
+	 */
+	public static <T> PagedResult<T>  newPagedResult( int perPage, long elementCount, int currentPage, List<T> pageElements, int realPerPage, int realCurrentPage, String virtualKey ) {
+		int pageCount = calcPageCount( elementCount, perPage );
+		AbstractPagedResult<T> result = new DefaultPagedResult<T>( perPage, elementCount, currentPage, pageCount, pageElements, realPerPage, realCurrentPage, virtualKey );
+		return result;
+	}
+	
+	/*
+	 * PagedResult per esisti negativi.
+	 * 
+	 * @param &lt;T&gt;
+	 * @param resultCode
+	 * @return
+	 */
+	protected static <T>  PagedResult<T>  newPagedResult( int resultCode ) {
+		DefaultPagedResult<T> result = new DefaultPagedResult<T>( -1, -1, -1, -1, null, -1, -1, null );
+		result.setResultCode( resultCode );
+		return result;
+	}
+	
+	protected DefaultPagedResult(int perPage, long elementCount, int currentPage, int pageCount, List<T> pageElements, int realPerPage, int realCurrentPage, String virtualKey) {
+		super(perPage, elementCount, currentPage, pageCount, pageElements);
+		this.realCurrentPage = realCurrentPage;
+		this.realPerPage = realPerPage;
+		this.virtualKey = virtualKey;
+	}
+
+	private static int calcPageCount( long elementCount, int perPage ) {
+		int pageCount = 0;
+		if(perPage>0)
+		{
+			pageCount = (int) ( ( elementCount )/( perPage ) );
+			if ( ( elementCount )%( perPage ) != 0 ) {
+				pageCount++;
+			}	
+		}
+		return pageCount;
+	}
+
+
+	@Override
+	public String getVirtualSearchKey() {
+		return this.virtualKey;
+	}
+
+	@Override
+	public Integer getRealPerPage() {
+		return this.realPerPage;
+	}
+	
+	@Override
+	public Integer getRealCurrentPage() {
+		return this.realCurrentPage;
+	}
+
+	@Override
+	public PagedResult<T> getVirtualPage( int currentPage ) {
+		int offset =  ((this.getRealCurrentPage()-1)*this.getRealPerPage());
+		int virtualStart = (currentPage-1)*this.getPerPage()-offset;
+		int virtualEnd = virtualStart+this.getPerPage();
+		this.getLogger().debug( "current page : "+currentPage+" size : "+this.getCurrentPageSize()+" vs : "+virtualStart+" ve : "+virtualEnd+" rps:"+this.getRealPerPage()+" , rp:"+this.getRealCurrentPage() );
+		List<T> elements = this.getPageElementsList().subList( virtualStart , virtualEnd );
+		return newPagedResult( this.getPerPage(), this.getElementCount(), currentPage, elements );
+	}
+
+	@Override
+	public boolean isFullResult() {
+		return false;
+	}
+	
+	
+}
---
+++ fj-core/src/main/java/org/fugerit/java/core/util/result/PagedResult.java
@@ -0,0 +1,163 @@
+package org.fugerit.java.core.util.result;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Inteface for handling paged result 
+ * 
+ * @author fugerit
+ *
+ * @param &lt;E&gt;
+ */
+public interface PagedResult<T> {
+
+	/*
+	 * Result code
+	 */
+	
+	public static final int RESULT_CODE_OK = 0;
+	
+	public static final int RESULT_CODE_KO = -1;
+	
+	public static final int FIRST_PAGE_INDEX = 1;
+	
+	/*
+	 * The method getElementCount() returns this value if the element count is unavalable
+	 */
+	public final static Integer ELEMENT_COUNT_UNAVAILABLE = -1;
+	
+	/*
+	 * <p>The position of the first element of the current pages ( (currentPage-1) * perPage )</p> 
+	 * 
+	 * @return	offset of the first element in this page
+	 */
+	public Integer getOffset();
+	
+	/*
+	 * <p>Maximum number of elements in a page</p>
+	 * 
+	 * @return	maximum number of elements in a page
+	 */
+	public Integer getPerPage();
+
+	/*
+	 * <p>Total number of elements in all pages</p>
+	 * 
+	 * @return	total number of elements in all pages
+	 */
+	public Long getElementCount();
+	
+	/*
+	 * <p>Position of current page ( in the range 1 - n )</p>
+	 * 
+	 * @return	position of current page
+	 */
+	public Integer getCurrentPage();	
+	
+	/*
+	 * <p>Total number of pages</p>
+	 * 
+	 * @return	total number of pages
+	 */
+	public Integer getPageCount();	
+	
+	/*
+	 * <p>Number of elements in current page</p>
+	 * 
+	 * @return	the size of the current page
+	 */
+	public Integer getCurrentPageSize();
+	
+	/*
+	 * <p>Elements in the current page</p>
+	 * 
+	 * @return	elements in the current page
+	 */
+	public Iterator<T> getPageElements();
+
+	/*
+	 * <p>Elements in the current page</p>
+	 * 
+	 * @return	elements in the current page
+	 */
+	public List<T> getPageElementsList();
+	
+	/*
+	 * <p>Iterator over page numbers ( 1 - n )</p>
+	 * 
+	 * @return	iterator over page numbers ( 1 - n )
+	 */
+	public Iterator<Integer> getPageCountIterator();
+	
+	/*
+	 * Result code for this page
+	 * 
+	 * @return result code
+	 */
+	public int getResultCode();
+	
+	/*
+	 * Additional info of this page.
+	 * 
+	 * @return	info
+	 */
+	public Map<String, Object> getInfo();
+	
+	
+	/* 
+	 * <code>true</code> if this is the last page.
+	 * 
+	 * @return <code>true</code> if it's the last page
+	 */
+	public boolean isLastPage();
+	
+	/* 
+	 * <code>true</code> if this is the last page.
+	 * 
+	 * @return <code>true</code> if it's the first page
+	 */
+	public boolean isFirstPage();	
+	
+	
+	// ******* additiona method for virtual paging *******
+	
+	// *******************************************************************************************
+	// *******************************************************************************************
+	// * VIRTUAL PAGING IS A METHOD WHERE A VIRTUAL PAGE IS MAPPED INTO A BIGGER PAGE            *
+	// *******************************************************************************************
+	// *******************************************************************************************
+	
+	/*
+	 * Virtual search key
+	 * 
+	 * @return	the virtual search key
+	 */
+	public String getVirtualSearchKey();
+	
+	public Integer getRealPerPage();
+	
+	public Integer getRealCurrentPage();
+	
+	public PagedResult<T> getVirtualPage( int currentPage );
+	
+	public boolean isSupportVirtualPaging();
+	
+	
+	// ******* additiona method for full result *******
+	
+	// *******************************************************************************************
+	// *******************************************************************************************
+	// * FULL RESULT IS A METHOD OF ACCESS PAGE WHERE ALL ELEMENTS ARE ACCESSIBLE ONLY ITERATING *
+	// *******************************************************************************************
+	// *******************************************************************************************
+	
+	/*
+	 * <code>true</code> if the the page contains the full result
+	 * 
+	 * @return
+	 */
+	public boolean isFullResult();
+
+}
