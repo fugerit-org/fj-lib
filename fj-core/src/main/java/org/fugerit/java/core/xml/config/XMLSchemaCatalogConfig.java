e5ee2bbdb4cdac2a490205b593799671bbb9c590
Daneel <d@fugerit.org>
2018-02-14 14:10:14 +0100

    Added support for resolving xsds

--- fj-core/src/main/java/org/fugerit/java/core/xml/config/XMLSchemaCatalogConfig.java
+++ fj-core/src/main/java/org/fugerit/java/core/xml/config/XMLSchemaCatalogConfig.java
@@ -1,8 +1,19 @@
 package org.fugerit.java.core.xml.config;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.Iterator;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.fugerit.java.core.cfg.ConfigException;
 import org.fugerit.java.core.cfg.xml.DataListCatalogConfig;
+import org.fugerit.java.core.lang.helpers.ClassHelper;
+import org.fugerit.java.core.lang.helpers.StringUtils;
+import org.w3c.dom.Element;
 
 public class XMLSchemaCatalogConfig extends DataListCatalogConfig {
 
@@ -23,5 +34,51 @@ public class XMLSchemaCatalogConfig extends DataListCatalogConfig {
 	 */
 	private static final long serialVersionUID = 6067071761917625426L;
 
+	public static final String CONFIG_PATH_MODE = "path-mode";
+	public static final String CONFIG_PATH_MODE_FILE = "file";
+	public static final String CONFIG_PATH_MODE_CLASSLOADER = "class-loader";
+	public static final String CONFIG_PATH_MODE_DEFAULT = CONFIG_PATH_MODE_CLASSLOADER;
+	
+	public static final String CONFIG_PATH_BASE = "path-base";
+	
+	private String pathMode;
+	private String pathBase;
+	
+	@Override
+	public void configure(Element tag) throws ConfigException {
+		super.configure(tag);
+		this.pathMode = StringUtils.valueWithDefault( tag.getAttribute( CONFIG_PATH_MODE ), CONFIG_PATH_MODE_DEFAULT );
+		this.pathBase = tag.getAttribute( CONFIG_PATH_BASE );
+		logger.info( "configure() "+CONFIG_PATH_MODE+"="+this.pathMode );
+		logger.info( "configure() "+CONFIG_PATH_BASE+"="+this.pathBase );
+	}
+
+	public Source[] getXsds( String setId ) {
+		return getXsds( this.pathMode, this.pathBase, this.getDataList( setId ) );
+	}
+	
+	public static Source[] getXsds( String pathMode, String pathBase, Collection<String> schemaList )  {
+		Source[] xsds = new Source[ schemaList.size() ];
+		Iterator<String> it = schemaList.iterator();
+		int k = 0;
+		while ( it.hasNext() ) {
+			try {
+				String current = it.next();
+				InputStream is = null;
+				if ( CONFIG_PATH_MODE_FILE.equalsIgnoreCase( pathMode ) ) {
+					File currentFile = new File( pathBase, current );
+					is = new FileInputStream( currentFile );
+				} else {
+					String fullPath = pathBase+"/"+current;
+					is =  ClassHelper.getDefaultClassLoader().getResourceAsStream( fullPath );
+				}
+				xsds[k] = new StreamSource( is );
+				k++;
+			} catch (Exception e) {
+				throw new RuntimeException( e );	
+			}
+		}
+		return xsds;
+	}
 
 }
