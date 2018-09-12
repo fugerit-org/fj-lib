f85aa3c56d04bc2259ab61944f95eb0b7579235f
Matteo Franci <mttfranci@gmail.com>
2018-09-12 10:17:42 +0200

    Added generic erro handler

---
+++ fj-core/src/main/java/org/fugerit/java/core/xml/sax/SAXErrorHandlerFail.java
@@ -0,0 +1,65 @@
+package org.fugerit.java.core.xml.sax;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * 
+ * @author mttfranci
+ *
+ */
+public class SAXErrorHandlerFail extends SAXErrorHandlerStore {
+
+	public static final int MODE_FAIL_ON_ANY_ERROR_STORE_WARNINGS = 2;
+	public static final int MODE_FAIL_ON_ANY_ERROR_IGNORE_WARNINGS = 3;
+	public static final int MODE_DEFAULT = MODE_FAIL_ON_ANY_ERROR_STORE_WARNINGS;
+	
+	public static SAXErrorHandlerFail newInstanceStoreOnly() {
+		return new SAXErrorHandlerFail();
+	}
+	
+	public static SAXErrorHandlerFail newInstanceFailOnAnyErrorStoreWarnings() {
+		return new SAXErrorHandlerFail() {
+			@Override
+			public void error(SAXParseException e) throws SAXException {
+				super.error(e);
+				throw e;
+			}
+			@Override
+			public void fatalError(SAXParseException e) throws SAXException {
+				super.fatalError(e);
+				throw e;
+			}
+		};
+	}
+
+	public static SAXErrorHandlerFail newInstanceFailOnFatakErrorStoreOthers() {
+		return new SAXErrorHandlerFail() {
+			@Override
+			public void fatalError(SAXParseException e) throws SAXException {
+				super.fatalError(e);
+				throw e;
+			}
+		};
+	}
+	
+	public static SAXErrorHandlerFail newInstanceFailOnAnyErrorIgnoreWarnings() {
+		return new SAXErrorHandlerFail() {
+			@Override
+			public void error(SAXParseException e) throws SAXException {
+				super.error(e);
+				throw e;
+			}
+			@Override
+			public void fatalError(SAXParseException e) throws SAXException {
+				super.fatalError(e);
+				throw e;
+			}
+			@Override
+			public void warning(SAXParseException e) throws SAXException {
+				super.warning( e );
+			}
+		};
+	}
+	
+}
---
+++ fj-core/src/main/java/org/fugerit/java/core/xml/sax/SAXErrorHandlerStore.java
@@ -0,0 +1,65 @@
+package org.fugerit.java.core.xml.sax;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class SAXErrorHandlerStore implements ErrorHandler {
+
+	private Collection<SAXException> errors;
+	private Collection<SAXException> fatals;
+	private Collection<SAXException> warnings;
+	
+	public SAXErrorHandlerStore() {
+		this.errors = new ArrayList<SAXException>();
+		this.fatals = new ArrayList<SAXException>();
+		this.warnings = new ArrayList<SAXException>();
+	}
+	
+	@Override
+	public void error(SAXParseException e) throws SAXException {
+		this.errors.add( e );
+	}
+
+	@Override
+	public void fatalError(SAXParseException e) throws SAXException {
+		this.fatals.add( e );
+	}
+
+	@Override
+	public void warning(SAXParseException e) throws SAXException {
+		this.warnings.add( e );
+	}
+
+	public Collection<SAXException> getErrors() {
+		return errors;
+	}
+
+	public Collection<SAXException> getFatals() {
+		return fatals;
+	}
+
+	public Collection<SAXException> getWarnings() {
+		return warnings;
+	}
+
+	public int getErrorSize() {
+		return this.getErrors().size();
+	}
+	
+	public int getFatalSize() {
+		return this.getFatals().size();
+	}
+	
+	public int getWarningSize() {
+		return this.getWarnings().size();
+	}
+	
+	public int getAllErrorsSize() {
+		return this.getErrorSize() + this.getFatalSize();
+	}
+	
+}
