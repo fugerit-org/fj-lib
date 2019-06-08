ef106d69183f959118d2fc9692fff5aa95096532
Daneel <d@fugerit.org>
2019-06-08 18:09:03 +0200

    Mini filter now work in a caching mode too

--- fj-core/src/main/java/org/fugerit/java/core/util/filterchain/MiniFilterConfig.java
+++ fj-core/src/main/java/org/fugerit/java/core/util/filterchain/MiniFilterConfig.java
@@ -2,7 +2,9 @@ package org.fugerit.java.core.util.filterchain;
 
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.fugerit.java.core.cfg.xml.GenericListCatalogConfig;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
@@ -16,6 +18,13 @@ public class MiniFilterConfig extends GenericListCatalogConfig<MiniFilterConfigE
 	 * 
 	 */
 	private static final long serialVersionUID = 286844409632297876L;
+
+	private Map<String, MiniFilterChain> mapChain;
+	
+	public MiniFilterConfig() {
+		super();
+		this.mapChain = new HashMap<String, MiniFilterChain>();
+	}
 
 	public static MiniFilterConfig loadConfig( InputStream is, MiniFilterConfig config ) throws Exception {
 		Document doc = DOMIO.loadDOMDoc( is );
@@ -38,5 +47,14 @@ public class MiniFilterConfig extends GenericListCatalogConfig<MiniFilterConfigE
 		} 
 		return chain;
 	}
+	
+	public MiniFilterChain getChainCache( String id ) throws Exception {
+		MiniFilterChain chain = this.mapChain.get( id );
+		if ( chain == null ) {
+			chain = this.getChain( id );
+			this.mapChain.put( id , chain );
+		}
+		return chain;
+	}	
 	
 }
