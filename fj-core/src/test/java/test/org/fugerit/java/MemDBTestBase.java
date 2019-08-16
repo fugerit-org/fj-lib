3f0b5533c91cc184835630695b505b05736d07a5
Daneel <d@fugerit.org>
2019-08-16 11:07:58 +0200

    junit optimization

--- fj-core/src/test/java/test/org/fugerit/java/MemDBTestBase.java
+++ fj-core/src/test/java/test/org/fugerit/java/MemDBTestBase.java
@@ -1,18 +1,20 @@
 package test.org.fugerit.java;
 
+import static org.junit.Assert.fail;
+
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.fugerit.java.core.db.connect.ConnectionFactory;
 import org.fugerit.java.core.db.connect.ConnectionFactoryImpl;
-import org.fugerit.java.core.db.dao.DAOException;
 import org.fugerit.java.core.db.helpers.SQLScriptFacade;
 import org.fugerit.java.core.db.helpers.SQLScriptReader;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
 import org.fugerit.java.core.util.PropsIO;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class MemDBTestBase extends BasicTest {
 
@@ -21,43 +23,55 @@ public class MemDBTestBase extends BasicTest {
 
 	private Connection conn;
 
-	public MemDBTestBase(ConnectionFactory cf) throws DAOException {
-		this.conn = cf.getConnection();
-	}
-
-	public MemDBTestBase(String path) throws Exception {
-		Properties props = PropsIO.loadFromClassLoader(path);
-		ConnectionFactory cf = ConnectionFactoryImpl.newInstance(props);
-		this.conn = cf.getConnection();
-	}
-
-	public MemDBTestBase() throws Exception {
-		this(DEFAULT_DB_CONN_PATH);
+	private static ConnectionFactory cf;
+	
+	public MemDBTestBase() {
+		try {
+			this.conn = cf.getConnection();
+		} catch ( Exception e) {
+			throw new RuntimeException( "Init error "+e, e );
+		}
 	}
 
 	public Connection getConnection() {
 		return this.conn;
 	}
 
-    @Before
-    public void create() throws Exception
+    @BeforeClass
+    public static void create() throws Exception
     {
-		try (InputStream is = ClassHelper.loadFromDefaultClassLoader(DEFAULT_DB_INIT_PATH);
-
-				SQLScriptReader reader = new SQLScriptReader(is)) {
-			int res = SQLScriptFacade.executeAll(reader, this.getConnection());
-			logger.info("initDB() total exec script script : " + res);
-		}
-    }
-
-    @After
-    public void destroy() throws Exception {
-		conn.close();
-    }    
+    	if ( cf == null ) {
+        	Properties props = PropsIO.loadFromClassLoader( DEFAULT_DB_CONN_PATH );
+    		cf = ConnectionFactoryImpl.newInstance(props);	
+    		try (InputStream is = ClassHelper.loadFromDefaultClassLoader(DEFAULT_DB_INIT_PATH);
+    				SQLScriptReader reader = new SQLScriptReader(is);
+    				Connection conn = cf.getConnection() ) {
+    			int res = SQLScriptFacade.executeAll(reader, conn);
+    			logger.info("initDB() total exec script script : " + res);
+    		}
+    	}
+    } 
 
 	@Override
 	protected void finalize() throws Throwable {
-		this.conn = null;
+
 	}
 
+	public void simpleTestSelectWorker( String select ) {
+		try ( Connection conn = this.getConnection();
+				Statement stm = conn.createStatement();
+				ResultSet rs = stm.executeQuery( select ) ) {
+			int read = 0;
+			while ( rs.next() ) {
+				read++;
+			}
+			logger.info( "total record read from table : "+read );
+		} catch (Exception e) {
+			String message = "Error : "+e;
+			logger.error( message, e );
+			e.printStackTrace();
+			fail( message );
+		}
+	}
+	
 }
