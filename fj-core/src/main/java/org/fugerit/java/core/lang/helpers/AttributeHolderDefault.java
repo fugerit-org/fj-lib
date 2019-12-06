51de90ba1d19572833dc88af5abe3de961202d08
Daneel <d@fugerit.org>
2019-12-06 12:17:59 +0100

    Added attribute holder default Removed deprecation

---
+++ fj-core/src/main/java/org/fugerit/java/core/lang/helpers/AttributeHolderDefault.java
@@ -0,0 +1,44 @@
+package org.fugerit.java.core.lang.helpers;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class AttributeHolderDefault implements Serializable, AttributesHolder {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -230640776936395216L;
+
+	private Map<String, Object> map;
+	
+	public AttributeHolderDefault() {
+		this.map = new HashMap<String, Object>();
+	}
+
+	public boolean containsAttribute(String key) {
+		return map.containsKey(key);
+	}
+
+	@Override
+	public Object getAttribute( String key ) {
+		return map.get(key);
+	}
+
+	@Override
+	public void setAttribute(String key, Object value) {
+		 map.put(key, value);
+	}
+
+	public Object removeAttribute(Object key) {
+		return map.remove(key);
+	}
+	
+	public Set<String> keySet() {
+		return map.keySet();
+	}	
+	
+}
