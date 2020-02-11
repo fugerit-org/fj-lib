47acf8a8fdde6081ad31d8490adcbc62120f2d2a
Daneel <d@fugerit.org>
2020-02-11 12:04:50 +0100

    Added support for queryView in DAOGEN

--- fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicRSExtractor.java
+++ fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicRSExtractor.java
@@ -5,9 +5,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.fugerit.java.core.db.dao.RSExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class BasicRSExtractor<T> implements RSExtractor<T>, Serializable {
 
+	protected static Logger logger = LoggerFactory.getLogger( BasicRSExtractor.class );
+	
 	/**
 	 * 
 	 */
