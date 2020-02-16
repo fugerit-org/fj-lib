42f18eff9a6f74df1e98c2b296bb4b92662aa6cd
Fugerit <m@fugerit.org>
2020-02-16 21:32:38 +0100

    javadoc

--- fj-core/src/main/java/org/fugerit/java/core/lang/compare/CheckEmpty.java
+++ fj-core/src/main/java/org/fugerit/java/core/lang/compare/CheckEmpty.java
@@ -2,6 +2,11 @@ package org.fugerit.java.core.lang.compare;
 
 public interface CheckEmpty {
 
+	/**
+	 * Check if the item is logically empty
+	 * 
+	 * @return	true if the item is empty
+	 */
 	boolean isEmpty();
 	
 }
