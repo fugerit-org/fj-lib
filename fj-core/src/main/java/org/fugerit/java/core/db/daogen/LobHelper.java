da8bbe85b3d31f111a4c95dea238f67adb337573
Daneel <d@fugerit.org>
2020-11-21 23:00:58 +0100

    0.7.3.11 (2020-11-21) -------------------- + Added daogen LobHelper class

---
+++ fj-core/src/main/java/org/fugerit/java/core/db/daogen/LobHelper.java
@@ -0,0 +1,28 @@
+package org.fugerit.java.core.db.daogen;
+
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class LobHelper {
+
+	public static Clob createClob( Connection conn, CharArrayDataHandler dh ) throws SQLException {
+		Clob c = null;
+		if ( dh != null ) {
+			c = conn.createClob();
+			c.setString(1, new String( dh.getData() ) );
+		}
+		return c;
+	}
+	
+	public static Blob createBlob( Connection conn, ByteArrayDataHandler dh ) throws SQLException {
+		Blob b = null;
+		if ( dh != null ) {
+			b = conn.createBlob();
+			b.setBytes(1, dh.getData());
+		}
+		return b;
+	}
+	
+}
