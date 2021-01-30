08196c482f746906db847a1bad0b59ee56879e95
Matteo a.k.a. Fugerit <m@fugerit.org>
2021-01-30 22:37:06 +0100

    JUnit for optional / required ValidatorRegex

--- fj-core/src/test/java/test/org/fugerit/java/core/validator/TestValidatorCatalog.java
+++ fj-core/src/test/java/test/org/fugerit/java/core/validator/TestValidatorCatalog.java
@@ -53,6 +53,8 @@ public class TestValidatorCatalog extends BasicTest {
 		this.validatorWorker( "testRegexValidator" , result, l, "test value one", "test label 1 (valid)", true, params);
 		this.validatorWorker( "testRegexValidatorExt" , result, l, "test value one", "test label 1 (not valid)", false, params);
 		this.validatorWorker( "testRegexValidatorExt" , result, l, "t", "t (not valid)", false, params);
+		this.validatorWorker( "testRegexValidatorExt" , result, l, "", "blank (required)", false, params);
+		this.validatorWorker( "testRegexValidatorExtOptional" , result, l, "", "blank (optional)", true, params);
 		this.validatorWorker( "testRegexValidator" , result, l, "test value 2 <>", "test label 2 (not valid)", false, params);
 		this.validatorWorker( "testDateValidator" , result, l, "01/03/2020", "Valid date", true, params);
 		this.validatorWorker( "testDateValidator" , result, l, "01/03-202A", "Invalid date 1", false, params);
