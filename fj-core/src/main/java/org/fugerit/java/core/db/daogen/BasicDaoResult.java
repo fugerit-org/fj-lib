b18d7b3fbb723b023ef534feca37ae8cf698ffa6
Matteo a.k.a. Fugerit <m@fugerit.org>
2021-03-05 08:25:18 +0100

    0.7.5.5 (2021-03-05)
    --------------------
    + Added DaoResult and DaoResultList interface

--- fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicDaoResult.java
+++ fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicDaoResult.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 import org.fugerit.java.core.util.result.BasicResult;
 
-public class BasicDaoResult<T> extends BasicResult {
+public class BasicDaoResult<T> extends BasicResult implements DaoResultList<T> {
 
 	public static final int RESULT_NODATAFOUND = -3;
 	public static final int RESULT_NOT_SET = Integer.MIN_VALUE;
+	
+	public BasicDaoResult(int resultCode, String resultDescription) {
+		this( resultCode );
+		this.resultDescription = resultDescription;
+	}
 	
 	public BasicDaoResult(int resultCode) {
 		super(resultCode);
@@ -21,20 +26,24 @@ public class BasicDaoResult<T> extends BasicResult {
 	
 	private List<T> list;
 
+	@Override
 	public List<T> getList() {
 		return list;
 	}
 
 	private String resultDescription;
 	
+	@Override
 	public String getResultDescription() {
 		return this.resultDescription;
 	}
 	
+	@Override
 	public void setResultDescription( String v ) {
 		this.resultDescription = v;
 	}
 	
+	@Override
 	public void evaluateResultFromList() {
 		if ( this.getList().isEmpty() ) {
 			this.setResultCode( RESULT_NODATAFOUND );
@@ -43,11 +52,18 @@ public class BasicDaoResult<T> extends BasicResult {
 		}
 	}
 	
+	@Override
+	public void setResult( DaoResult result)  {
+		this.setResult( result.getResultCode(), result.getResultDescription() );
+	}
+	
+	@Override
 	public void setResult( int resultCode, String resultDescription )  {
 		this.setResultCode( resultCode );
 		this.setResultDescription( resultDescription );
 	}
 	
+	@Override
 	public void setSingleResult( T value ) {
 		this.getList().set( 0 , value );
 	}
@@ -59,6 +75,7 @@ public class BasicDaoResult<T> extends BasicResult {
 	 * 
 	 * @return	the first element in the result list or null if empty
 	 */
+	@Override
 	public T getSingleResult() {
 		T res = null;
 		if ( this.getList().size() == 1 ) {
---
+++ fj-core/src/main/java/org/fugerit/java/core/db/daogen/DaoResult.java
@@ -0,0 +1,15 @@
+package org.fugerit.java.core.db.daogen;
+
+import org.fugerit.java.core.util.result.Result;
+
+public interface DaoResult extends Result {
+
+	String getResultDescription();
+	
+	void setResultDescription( String v );
+	
+	void setResult( DaoResult result);
+	
+	void setResult( int resultCode, String resultDescription );
+	
+}
