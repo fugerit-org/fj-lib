4711034738f60f8d97c0f9971bec087446cf1628
Matteo Franci <m@fugerit.org>
2022-11-24 20:49:42 +0100

    Upgraded I18N helper with test cases

--- fj-core/src/main/java/org/fugerit/java/core/util/i18n/HelperI18N.java
+++ fj-core/src/main/java/org/fugerit/java/core/util/i18n/HelperI18N.java
@@ -2,43 +2,21 @@ package org.fugerit.java.core.util.i18n;
 
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
 
-import org.fugerit.java.core.lang.helpers.StringUtils;
-
-public class HelperI18N {
-	
-	private static void createBundleMap( Map<String, ResourceBundle> bundleMap, String baseName, String defaultLanguge, String... altLocale ) {
-		bundleMap.put( defaultLanguge , ResourceBundle.getBundle( baseName, Locale.forLanguageTag( defaultLanguge ), new XMLResourceBundleControl() ) );
-		for ( int k=0; k<altLocale.length; k++ ) {
-			bundleMap.put( altLocale[k] , ResourceBundle.getBundle( baseName, Locale.forLanguageTag( altLocale[k] ), new XMLResourceBundleControl() ) );
-		}
-	}
-	
-	public static HelperI18N newHelperI18N( String baseName, String defaultLanguge,  String... altLocale ) {
-		HelperI18N helper = new HelperI18N(baseName, defaultLanguge, altLocale);
-		return helper;
-	}
-	
+public abstract class HelperI18N {
+		
 	protected HelperI18N( String baseName, String defaultLanguge,  String... altLocale ) {
 		this.defaultLanguage = defaultLanguge;
 		this.altLocale = altLocale;
 		if ( this.altLocale == null ) {
 			this.altLocale = new String[0];
 		}
-		this.bundleMap = new HashMap<String, ResourceBundle>();
-		createBundleMap(this.bundleMap, baseName, defaultLanguge, altLocale);
 	}
 
 	private String defaultLanguage;
 	
 	private String[] altLocale;
-	
-	private Map<String, ResourceBundle> bundleMap;
 	
 	public String getDefaultLanguage() {
 		return defaultLanguage;
@@ -48,35 +26,22 @@ public class HelperI18N {
 		return Arrays.asList( this.altLocale );
 	}
 
-	public Map<String, ResourceBundle> getBundleMap() {
-		return bundleMap;
-	}
-
-	public ResourceBundle findBundle( String lang ) {
-		ResourceBundle bundle = this.bundleMap.get( this.getDefaultLanguage() );
-		if ( StringUtils.isNotEmpty( lang ) ) {
-			bundle = bundleMap.get( lang );
-		}
-		return bundle;
-	}
+	public abstract String resolveString( String lang, String key );
 	
 	public String getString( String lang, String key, Object... params ) {
-		ResourceBundle bundle = findBundle( lang );
-		String msg = null;
-		if ( bundle != null ) {
-			msg = bundle.getString( key );
-			if ( msg != null && params != null && params.length > 0 ) {
-				Object[] realParams = new Object[ params.length ];
-				for ( int k=0; k<params.length; k++ ) {
-					Object current = params[k];
-					if ( current instanceof ParamI18N )  {
-						current = bundle.getString( ((ParamI18N)current).getKey() );			
-					}
+		String message = this.resolveString(lang, key);
+		if ( message != null && params != null && params.length > 0 ) {
+			Object[] realParams = new Object[ params.length ];
+			for ( int k=0; k<params.length; k++ ) {
+				Object current = params[k];
+				if ( current instanceof ParamI18N )  {
+					current = this.resolveString( lang, ((ParamI18N)current).getKey() );			
 				}
-				msg = MessageFormat.format( msg, realParams );
+				realParams[k] = current;
 			}
+			message = MessageFormat.format( message, realParams );
 		}
-		return msg;
+		return message;
 	}
 	
 }
--- fj-core/src/test/java/test/org/fugerit/java/core/util/i18n/TestHelperI18N.java
+++ fj-core/src/test/java/test/org/fugerit/java/core/util/i18n/TestHelperI18N.java
@@ -1,6 +1,9 @@
 package test.org.fugerit.java.core.util.i18n;
 
+import org.fugerit.java.core.util.i18n.BundleMapI18N;
 import org.fugerit.java.core.util.i18n.HelperI18N;
+import org.fugerit.java.core.util.i18n.ParamI18N;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -22,20 +25,48 @@ public class TestHelperI18N {
 	
 	private HelperI18N helper;
 	
-	private void testHelper( String lang, String key, Object... params ) {
+	private void testHelper( String expectedValue, String lang, String key, Object... params ) {
 		String value = this.helper.getString( lang , key, params );
 		logger.info( "key:{} , lang:{} -> {}", lang, key, value );
+		Assert.assertEquals( "Value different for key : "+key+", lang : "+lang , expectedValue, value );
 	}
 	
 	@Before
 	public void init() {
-		this.helper = HelperI18N.newHelperI18N( CONF_PATH, LANG_DEF, LANG_ALT );
+		this.helper = BundleMapI18N.newHelperI18N( CONF_PATH, LANG_DEF, LANG_ALT );
 	}
 	
 	@Test
-	public void testSimpleProperty() {
-		this.testHelper(  LANG_DEF,  "test.prop.1" );
-		this.testHelper(  LANG_ALT[0],  "test.prop.1" );
+	public void testSimpleProperty1() {
+		String key = "test.prop.1"; // test.prop.1 is available for both languages
+		this.testHelper( "Test property 1", LANG_DEF,  key );
+		this.testHelper( "Proprietà test 1", LANG_ALT[0],  key );
 	}
+	
+	@Test
+	public void testSimpleProperty2() {
+		String key = "test.prop.2";	// test.prop.2 is available only for default language
+		this.testHelper( "Test property 2", LANG_DEF,  key );
+		this.testHelper( "Test property 2", LANG_ALT[0],  key );
+	}
+	
+	@Test
+	public void testSimpleProperty3() {
+		String key = "test.prop.3";	// test.prop.3 is available only for default language
+		this.testHelper( null, LANG_DEF, key  );
+		this.testHelper( "Proprietà test 3", LANG_ALT[0], key );
+	}
+	
+	@Test
+	public void testComplexProperty1() {
+		String key = "test.complex.prop.1";	// contains a integer param and a i18n param
+		String keyParam = "test.param.1";	// the key for the i18n param
+		this.testHelper( "Complex property 1 -> simple parameter : 3 and i18n parameter : (ParamI18N value)", 
+				LANG_DEF,  key, new Integer( 3 ), ParamI18N.newParamI18N( keyParam ) );
+		this.testHelper( "Proprietà complessa test 1 -> parametro semplice : 3 e parametro i18n : (valore ParamI18N)", 
+				LANG_ALT[0],  key, new Integer( 3 ), ParamI18N.newParamI18N( keyParam ) );
+	}
+	
+	
 		
 }
