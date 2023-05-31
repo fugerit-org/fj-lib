d4ebdc04c95492a07196f21b070d3306717aa54c
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-05-31 07:08:51 +0200

    Added native profile

---
+++ fj-core/src/main/java/org/fugerit/java/core/MainNative.java
@@ -0,0 +1,14 @@
+package org.fugerit.java.core;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainNative {
+
+	public static void main( String[] args ) {
+		log.info( "test start" );
+		System.out.println( "TEST" );
+		log.info( "test end" );
+	}
+	
+}
