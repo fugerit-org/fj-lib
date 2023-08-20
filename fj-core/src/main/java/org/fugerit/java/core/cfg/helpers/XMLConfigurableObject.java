9f4c7e59dc34eb10e386fc46df0605c89170e9df
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-20 13:40:11 +0200

    + [enhancement][fj-core] Added factory methods for XMLConfigurableObject

--- fj-core/src/main/java/org/fugerit/java/core/cfg/helpers/XMLConfigurableObject.java
+++ fj-core/src/main/java/org/fugerit/java/core/cfg/helpers/XMLConfigurableObject.java
@@ -21,8 +21,11 @@
 package org.fugerit.java.core.cfg.helpers;
 
 import java.util.Properties;
+import java.util.function.Consumer;
 
 import org.fugerit.java.core.cfg.ConfigException;
+import org.fugerit.java.core.log.LogFacade;
+import org.w3c.dom.Element;
 
 /**
  * <p>Base class for implementations of ConfigurableObject supporting only
@@ -39,6 +42,18 @@ public abstract class XMLConfigurableObject extends AbstractConfigurableObject {
 	 */
 	private static final long serialVersionUID = -3032211194907648543L;
 
+	public static XMLConfigurableObject newXMLConfigurableObject( Consumer<Element> configureFun ) {
+		return new XMLConfigurableObject() {	
+			private static final long serialVersionUID = 7207919744441264730L;
+			@Override
+			public void configure(Element tag) throws ConfigException {
+				configureFun.accept(tag);
+			}
+		};
+	}
+	
+	public static final XMLConfigurableObject DO_NOTHING = newXMLConfigurableObject( e -> { LogFacade.getLog().debug( "do nothing impl , param element : {}" , e); } );
+				
 	/* (non-Javadoc)
 	 * @see org.fugerit.java.core.cfg.ConfigurableObject#configure(java.util.Properties)
 	 */
