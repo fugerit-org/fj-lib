531ffdba86e48331b0de77770742282dcf22ca31
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-23 16:33:36 +0200

    Fix major issue by sonar cloud

--- fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicWrapper.java
+++ fj-core/src/main/java/org/fugerit/java/core/db/daogen/BasicWrapper.java
@@ -1,10 +1,39 @@
 package org.fugerit.java.core.db.daogen;
 
+import java.io.IOException;
+import java.io.Serializable;
+
 import org.fugerit.java.core.lang.helpers.Wrapper;
 import org.fugerit.java.core.lang.helpers.WrapperHelper;
 
-public class BasicWrapper<T> implements Wrapper<T> {
+public class BasicWrapper<T> implements Wrapper<T>, Serializable {
 
+	private static final long serialVersionUID = -9201341852062067118L;
+	
+	// code added to setup a basic conditional serialization - START
+	
+	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+		// this class is conditionally serializable, depending on contained object
+		// special situation can be handleded using this method in future
+		out.defaultWriteObject();
+	}
+
+	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+		// this class is conditionally serializable, depending on contained object
+		// special situation can be handleded using this method in future
+		in.defaultReadObject();
+	}
+	
+	// code added to setup a basic conditional serialization - END
+	
+	protected static final boolean UNSOPPORTED_OPERATION = true;
+	
+	public static void throwUnsupported( String message ) {
+		if ( UNSOPPORTED_OPERATION ) {
+			throw new UnsupportedOperationException( message );
+		}
+	}
+	
 	private T wrapped;
 
 	@Override
