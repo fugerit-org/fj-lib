5d0b7aa08f148e29bedcbc74a11f2b02a9c222be
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-07-05 18:01:50 +0200

    typo in MavenProps utility, constant GROUP_ID value is "gropupId" should be "groupId" #97

--- fj-core/src/test/java/test/org/fugerit/java/core/util/mvn/TestMavenProps.java
+++ fj-core/src/test/java/test/org/fugerit/java/core/util/mvn/TestMavenProps.java
@@ -6,20 +6,23 @@ import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
+
 @Slf4j
 class TestMavenProps {
 
 	@Test
 	void testMavenOk() {
-		String prop = MavenProps.getPropery( "org.fugerit.java", "fj-core", "artifactId" );
-		log.info( "version {}", prop );
-		Assertions.assertEquals( "fj-core" , prop );
+		Optional prop = MavenProps.getPropertyOptional( "org.fugerit.java", "fj-core", "artifactId" );
+		log.info( "version ok {}", prop.get() );
+		Assertions.assertEquals( "fj-core" , prop.get() );
 	}
 	
 	@Test
 	void testMavenKo() {
+		// use deprecated method for coverage
 		String prop = MavenProps.getPropery( "org.fugerit.java.no.exists", "fj-core", "artifactId" );
-		log.info( "version {}", prop );
+		log.info( "version ko {}", prop );
 		Assertions.assertNull( prop );
 	}
 	
